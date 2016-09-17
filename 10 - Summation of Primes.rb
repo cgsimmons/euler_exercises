35c784773704672ddcef54e1b78270891e729452
cgsimmons <chris.simmons.alt@gmail.com>
2016-09-16 18:24:26 -0700

    Problem 10

---
+++ 10 - Summation of Primes.rb
@@ -0,0 +1,14 @@
+#The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+#Find the sum of all the primes below two million.
+require 'prime'
+def sumOfPrimes(max)
+  sum = 0
+  Prime.each() do |prime|
+    break if prime > max
+    sum += prime
+  end
+  sum
+end
+
+puts sumOfPrimes(2000000)
